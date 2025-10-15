416f6c4fbf34da9a403a5bbbffd62552f2177f4d
tamicaa-mat <tamicaa.mat@gmail.com>
2025-10-15 13:53:32 -0300

    Primer commit del proyecto

---
+++ src/main/java/com/apiProductos/cursoTDC/CursoTdcApplication.java
@@ -0,0 +1,13 @@
+package com.apiProductos.cursoTDC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoTdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoTdcApplication.class, args);
+	}
+
+}
